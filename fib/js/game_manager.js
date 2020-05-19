822cd858e2269f523b3fe201e77c5ab5f93ec1ea
TheReal23 <65376048+TheReal23@users.noreply.github.com>
2020-05-18 19:12:12 -0700

    add fib game

--- fib/js/game_manager.js
+++ fib/js/game_manager.js
@@ -121,7 +121,8 @@ GameManager.prototype.move = function (direction) {
 
           // Update the score
           self.score += merged.value;
-
+          // The mighty 2048 tile
+          if (merged.value === 6765) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
